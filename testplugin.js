0b993cc2d3ed4b40785932705af139f928b73515
GiganticBucket <91396768+GiganticBucket@users.noreply.github.com>
2023-02-01 23:58:58 -0800

    Update testplugin.js

--- testplugin.js
+++ testplugin.js
@@ -1,1 +1,1 @@
-registerPlugin("Sample Plugin from Github Pages", { TryHandle(message) { alert("The Github Pages plugin was notified of the message: " + message); return true; } });
+registerPlugin({name: "TestPlugin"});

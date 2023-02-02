47f78c5c0fb9ef199d0bb037d15d428564b83f14
GiganticBucket <91396768+GiganticBucket@users.noreply.github.com>
2023-02-02 10:50:47 -0800

    Update testplugin.js

--- testplugin.js
+++ testplugin.js
@@ -1,3 +1,15 @@
+let api = window["emoteWallAPI"];
+
 console.log("Hi from imported js 1");
-registerPlugin({name: "TestPlugin"});
+
+api.registerPlugin({name: "TestPlugin1"});
+
 console.log("Hi from imported js 2");
+
+window["emoteWallAPI"].registerPlugin({name: "TestPlugin2"});
+
+console.log("Hi from imported js 3");
+
+emoteWallAPI.registerPlugin({name: "TestPlugin3"});
+
+console.log("Hi from imported js 4");
